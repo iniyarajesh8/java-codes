efa875085cde996a6e4b20010d021c3f52519d8d
iniyarajesh8 <iniyarajesh8@gmail.com>
2026-04-01 18:19:22 +0400

    Add files via upload

---
+++ smallestlogical.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class smallestlogical {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter 1st number: ");
+        int x = sc.nextInt();
+        System.out.print("enter 2nd number: ");
+        int y = sc.nextInt();
+        System.out.print("enter 3rd number: ");
+        int z = sc.nextInt();
+
+        if((x<y)&&(x<z)){
+            System.out.print(x+" is the smallest number");
+        }
+
+        else if((y<x)&&(y<z)){
+            System.out.print(y+" is the smallest number");
+        }
+
+        else{
+            System.out.print(z+" is the smallest number");
+        }
+
+    }
+}
